99c46e4687132ad03da0fb80d3c1eb624bb5cdb9
Diego D <56505625+ddiazmendoza@users.noreply.github.com>
2022-01-12 16:21:46 -0600

    Track with Git LFS

--- YGOCardSearch/Pages/RandomCard.cshtml.cs
+++ YGOCardSearch/Pages/RandomCard.cshtml.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using YGOCardSearch.Models;
 
 namespace YGOCardSearch.Pages
 {
     public class RandomCardModel : PageModel
     {
-        public void GetRandomCard() 
+        Random random = new Random();
+        public CardModel Card {get; set; }
+        public ICardsProvider cardsProvider { get; set; }
+        public long CardId {get; set;}
+
+        public RandomCardModel(ICardsProvider cardsProvider)
         {
+            this.cardsProvider = cardsProvider;
         }
+        
+        public async Task<CardModel> GetCard() 
+        {
+            var randomCard = await cardsProvider.GetRandomCardAsync();
+            return randomCard;
+        }
+
+
         public async Task<IActionResult> OnGet()
         {
+            Card = await cardsProvider.GetRandomCardAsync();
+            CardId = Card.Id;
+
             return Page();
         }
     }
--- YugiohDB/Program.cs
+++ YugiohDB/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace YugiohDB
@@ -9,7 +12,20 @@ namespace YugiohDB
     {
         static async Task Main(string[] args)
         {
-            Start:
+            
+            var IdsList = await Connection.GetAllCardsIds();
+
+            var serializedList = JsonSerializer.Serialize(IdsList, new JsonSerializerOptions()) ;
+            
+            File.WriteAllText(@"C:\Users\d_dia\source\repos\YuGiOhTCG\YugiohDB\data\ids.txt", serializedList);
+            Console.WriteLine("\nTodas las cartas ids han sido guardadas exitosamente");
+
+
+            Console.WriteLine("IDS Created and saved");
+            Thread.Sleep(3000);
+            Environment.Exit(0); 
+
+        Start:
 
             Console.WriteLine("Write your search and press enter:");
             var search = Console.ReadLine();
@@ -45,6 +61,30 @@ namespace YugiohDB
             else 
             {
                 return null; 
+            }
+        }
+        public static async Task<List<long>> GetAllCardsIds() 
+        {
+            string url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?";
+
+            var ygoClient = new HttpClient() { BaseAddress = new Uri(url) };
+            var request = await ygoClient.GetAsync(url);
+            if (request.IsSuccessStatusCode)
+            {
+                var content = await request.Content.ReadAsStringAsync();
+                var model = JsonSerializer.Deserialize<ygoModel>(content, new JsonSerializerOptions());
+                Console.WriteLine("Cards found: " + model.Data.Length);
+                List<long> CardsIds = new List<long>();
+
+                foreach (var card in model.Data) 
+                {
+                    CardsIds.Add(card.Id);
+                }
+                return CardsIds;
+            }
+            else
+            {
+                return null;
             }
         }
     }
