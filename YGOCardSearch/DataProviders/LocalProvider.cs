786567470b3384e7130ff54094350f280d2929db
Diego D <56505625+diego-devs@users.noreply.github.com>
2022-02-27 12:52:22 -0600

    local repo for card builder

---
+++ YGOCardSearch/DataProviders/LocalProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using YGOCardSearch.Models;
+
+namespace YGOCardSearch.DataProviders
+{
+    public class LocalProvider
+    {
+
+       
+        
+    }
+}
