3f94a27923d012c05b245ffe9166f47b84343f78
Diego D <56505625+diego-devs@users.noreply.github.com>
2022-03-28 20:44:26 -0600

    working on DepInj for DBContext

---
+++ YGOCardSearch/Data/Models/ImageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace YGOCardSearch.Models
+{
+    public class ImageModel
+    {
+       
+        [Key]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("image_url")]
+        public Uri ImageUrl { get; set; }
+
+        [JsonPropertyName("image_url_small")]
+        public Uri ImageUrlSmall { get; set; }
+       
+
+    }
+}
