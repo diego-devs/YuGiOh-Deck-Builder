a6308388c1dbf02af5839caa403cbb6c5ce6112f
Diego D <56505625+diego-devs@users.noreply.github.com>
2022-04-06 20:31:36 -0500

    Trying to load data to database

---
+++ YugiohDB/Models/CardPrice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace YugiohDB.Models
+{
+    public class CardPrice
+    {
+        [Key]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("cardmarket_price")]
+        public string CardmarketPrice { get; set; }
+
+        [JsonPropertyName("tcgplayer_price")]
+        public string TcgplayerPrice { get; set; }
+
+        [JsonPropertyName("ebay_price")]
+        public string EbayPrice { get; set; }
+
+        [JsonPropertyName("amazon_price")]
+        public string AmazonPrice { get; set; }
+
+        [JsonPropertyName("coolstuffinc_price")]
+        public string CoolstuffincPrice { get; set; }
+
+    }
+}
---
+++ YugiohDB/YGOProvider.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using YugiohDB.Models;
+
+namespace YugiohDB ;
+public static class YGOProvider 
+    {
+        /// <summary>
+        /// Gets searched card or similar names
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        public static async Task<ResponseModel> SearchAsync(string search) 
+        {
+            string url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=" + search;
+            
+            var ygoClient = new HttpClient() {BaseAddress = new Uri(url)};
+            var request = await ygoClient.GetAsync(url);
+            if (request.IsSuccessStatusCode) 
+            {
+                var content = await request.Content.ReadAsStringAsync();
+                var model = JsonSerializer.Deserialize<ResponseModel>(content, new JsonSerializerOptions());
+                Console.WriteLine("Cards found: " + model.Data.Count);
+                return model;
+            }
+            else 
+            {
+                return null; 
+            }
+        }
+        /// <summary>
+        /// Gets searched card by TCG id
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<IEnumerable<Card>> GetAllCardsAsync() 
+        {
+            string url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?"; //Gets all cards!
+
+            var ygoClient = new HttpClient() { BaseAddress = new Uri(url) };
+            var request = await ygoClient.GetAsync(url);
+            if (request.IsSuccessStatusCode)
+            {
+                var content = await request.Content.ReadAsStringAsync();
+                var model = JsonSerializer.Deserialize<ResponseModel>(content, new JsonSerializerOptions());
+                Console.WriteLine("Cards found: " + model.Data.Count);
+                var allCards = model.Data;
+                return allCards;
+                
+            }
+            return null;
+        }
+
+       
+    }
