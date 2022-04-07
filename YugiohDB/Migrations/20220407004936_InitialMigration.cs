1ef7f0a077bab14ced5218c79bd12533c3ac1691
Diego D <56505625+diego-devs@users.noreply.github.com>
2022-04-06 20:31:36 -0500

    Trying to load data to database

---
+++ YugiohDB/Migrations/20220407004936_InitialMigration.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YugiohDB.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Atk = table.Column<long>(type: "bigint", nullable: false),
+                    Def = table.Column<long>(type: "bigint", nullable: false),
+                    Level = table.Column<long>(type: "bigint", nullable: false),
+                    Race = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Attribute = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CardId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cards_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetRarity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetRarityCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CardId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CardImage_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardPrices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardmarketPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TcgplayerPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EbayPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AmazonPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CoolstuffincPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CardId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardPrices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CardPrices_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardSets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetRarity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetRarityCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SetPrice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CardId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardSets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CardSets_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardImage_CardId",
+                table: "CardImage",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardPrices_CardId",
+                table: "CardPrices",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_CardId",
+                table: "Cards",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardSets_CardId",
+                table: "CardSets",
+                column: "CardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardImage");
+
+            migrationBuilder.DropTable(
+                name: "CardPrices");
+
+            migrationBuilder.DropTable(
+                name: "CardSets");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
--- YugiohDB/Program.cs
+++ YugiohDB/Program.cs
@@ -1,100 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using YugiohDB.Models;
 
 namespace YugiohDB
 {
-    public static class YugiohCards 
-    {
-        public static List<CardModel> Cards {get; set;}
-    }
     class Program
     {
         static async Task Main(string[] args)
         {
-            
-            
-            // Where cards are saved
+            // path to .txt
             var path = @"C:\Users\d_dia\source\repos\YuGiOhTCG\YugiohDB\data\allCards.txt";
-            System.IO.File.Create(path);
-
-            // Create all cards in a text file to /YGOCardSearch/data
-            var allCards = await Connection.GetAllCardsAsync();
-            YugiohCards.Cards = new List<CardModel>(allCards);
-
-
-            var jsonCards = JsonSerializer.Serialize<List<CardModel>>(YugiohCards.Cards, new JsonSerializerOptions());
-            
-            System.IO.File.WriteAllText(path, jsonCards);
-
-
-            Thread.Sleep(3000);
-            Environment.Exit(0); 
-
-        Start:
-
-            Console.WriteLine("Write your search and press enter:");
-            var search = Console.ReadLine();
-
-            var CardList = await Connection.SearchAsync(search);
-            foreach (var card in CardList.Data) 
+            // to string
+            var r = File.ReadAllText(path);
+            // Optiond fot serialization, hope to avoid [data] and other null values
+            JsonSerializerOptions options = new()
             {
-                Console.WriteLine($"{card.Name} / {card.Type} ");
-            }
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
 
-            Console.WriteLine("New search? y/n:");
-            var response = Console.ReadLine();
-            if (response == "y" || response == "Y") {goto Start;}
-            else if (response == "n" || response == "N") {Environment.Exit(0);}     
+            var readedCards = JsonSerializer.Deserialize<ICollection<Card>>(r, options);
+            var allCards = new List<Card>(readedCards);
+
+            var selectedCards = new List<Card>(allCards.Where(c => c.Atk == 1000)) ;
+
+            
+
+            // Connect to database in Artemis
+            using (var context = new YugiohContext())
+            {
+                context.AddRange(selectedCards);
+
+                context.SaveChanges();
+            }
         }
 
     }
-    public static class Connection 
-    {
-        public static async Task<CardModel> SearchAsync(string search) 
-        {
-            string url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=" + search;
-            
-            var ygoClient = new HttpClient() {BaseAddress = new Uri(url)};
-            var request = await ygoClient.GetAsync(url);
-            if (request.IsSuccessStatusCode) 
-            {
-                var content = await request.Content.ReadAsStringAsync();
-                var model = JsonSerializer.Deserialize<CardModel>(content, new JsonSerializerOptions());
-                Console.WriteLine("Cards found: " + model.Data.Length);
-                return model;
-            }
-            else 
-            {
-                return null; 
-            }
-        }
-        public static async Task<IEnumerable<CardModel>> GetAllCardsAsync() 
-        {
-            string url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?"; //Gets all cards!
-
-            var ygoClient = new HttpClient() { BaseAddress = new Uri(url) };
-            var request = await ygoClient.GetAsync(url);
-            if (request.IsSuccessStatusCode)
-            {
-                var content = await request.Content.ReadAsStringAsync();
-                var model = JsonSerializer.Deserialize<CardModel>(content, new JsonSerializerOptions());
-                Console.WriteLine("Cards found: " + model.Data.Length);
-                var allCards = model.Data;
-                return allCards;
-                
-            }
-            return null;
-        }
-
-        public static async void SaveAllCards() 
-        {
-            var allCards = await GetAllCardsAsync();
-            
-        }
-    }
+    
 }
