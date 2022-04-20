646f63f66d5bd9a07c2560f7bc361daefe8a74bb
diego-devs <d_diazmendoza@hotmail.com>
2022-04-20 13:55:11 -0500

    Track with Git LFS

---
+++ YGOCardSearch/Data/Models/CardImage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace YGOCardSearch.Data.Models
+{
+    [Keyless]
+    public class CardImage
+    {
+        [JsonPropertyName("image_url")]
+        public string ImageUrl { get; set; }
+
+        [JsonPropertyName("image_url_small")]
+        public string ImageUrlSmall { get; set; }
+
+    }
+}
