5c771c3a1fcd485bb7ea1cb0b186a88efba3e6d0
diego-devs <d_diazmendoza@hotmail.com>
2022-06-22 19:07:54 -0500

    added correct models to yugiohDb Console proj

---
+++ YugiohDB/Models/CardImage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace YGOCardSearch.Data.Models
+{
+    public class CardImage
+    {
+        [Key]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("image_url")]
+        public string ImageUrl { get; set; }
+
+        [JsonPropertyName("image_url_small")]
+        public string ImageUrlSmall { get; set; }
+    }
+}
