36ad8379780d7709af80cd8d376dc4412d8c5cbd
diego-devs <d_diazmendoza@hotmail.com>
2022-06-22 19:07:54 -0500

    Track with Git LFS

---
+++ YugiohDB/Models/SetModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace YGOCardSearch.Data.Models
+{
+    public class SetModel
+    {
+        [Key]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        
+        [JsonPropertyName("set_name")]
+        public string Name { get; set; }
+        [JsonPropertyName("set_code")]
+        public string Code { get; set; }
+        [JsonPropertyName("set_rarity")]
+        public string Rarity { get; set; }
+        [JsonPropertyName("set_rarity_code")]
+        public string RarityCode { get; set; }
+        [JsonPropertyName("set_price")]
+        public string Price { get; set; }
+    }
+}
