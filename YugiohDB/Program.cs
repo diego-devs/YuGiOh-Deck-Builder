241bf777b5d39bcb67bfc60a391d5f9f846f6d34
diego-devs <d_diazmendoza@hotmail.com>
2022-08-27 06:31:07 -0500

    DB project updated - Main method cleaned

--- YugiohDB/Program.cs
+++ YugiohDB/Program.cs
@@ -16,43 +16,42 @@ namespace YugiohDB
     {
         static async Task Main(string[] args)
         {
-            // path to .txt
+            
+        }
+
+        /// <summary>
+        /// Adds all cards from json file to Database
+        /// </summary>
+        /// <returns></returns>
+        public static async Task AddAllCards() 
+        {
+            // This is the process to add all cards to Database
+            // path to allcards.txt file json file
             var path = @"C:\Users\d_dia\source\repos\YuGiOhTCG\YugiohDB\data\allCards.txt";
-            // to string
             var r = File.ReadAllText(path);
-            // Optiond fot serialization, hope to avoid [data] and other null values
+            // Option for serialization, hoping to avoid null values
             JsonSerializerOptions options = new()
             {
                 DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
             };
-            
+            // all cards from json file deserialized to a list
             var readedCards = JsonSerializer.Deserialize<ICollection<CardModel>>(r, options);
-            
-
-            
-
-            
-
             // Connect to database in Artemis
             using (var context = new YugiohContext())
             //using (var transaction = context.Database.BeginTransaction())
             {
                 try
                 {
+                    // Add all the cards to database excecuting SQL commands and using EF
                     //context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [YgoDB].[dbo].[Cards] ON");
                     //context.Cards.AddRange(readedCards);
-                    
                     //context.SaveChanges();
                     //context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [YgoDB].[dbo].[Cards] OFF");
-                    
-                    
                     //transaction.Commit();
-
-
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Los cambios no pudieron hacerse");
+                    Console.WriteLine("Changes have not been made");
                     Console.WriteLine(e.Message);
                     Console.WriteLine(e.InnerException.Message);
                 }
@@ -60,7 +59,7 @@ namespace YugiohDB
                 {
                     Console.WriteLine($"{readedCards.Count} cards saved in {context.Database.ProviderName}");
                 }
-                
+
             }
         }
 
