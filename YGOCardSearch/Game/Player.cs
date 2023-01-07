998fc630bd4c1d619998f4edf5362c547313ce94
Diego Díaz Mendoza <d_diazmendoza@hotmail.com>
2023-01-07 01:57:59 -0600

    added first classes for game feature

---
+++ YGOCardSearch/Game/Player.cs
@@ -0,0 +1,31 @@
+﻿using YGOCardSearch.Data.Models;
+using YGOCardSearch.Game.GameZones;
+
+namespace YGOCardSearch.Game
+{
+    public class Player
+    {
+        public static Deck Deck { get; set; }
+        public GameField GameField { get; set; }
+        public static int LifePoints { get; set; }
+        public GraveyardZone GraveyardZone { get; set; }
+        public HandZone HandZone { get; set; }
+        public MonsterZone MonsterZone { get; set; }
+        public MagicTrapZone MagicTrapZone { get; set; }
+        public DeckZone DeckZone { get; set; }
+        public ExtraDeckZone ExtraDeckZone { get; set; }
+
+        public static void Draw()
+        {
+            var c = DeckZone.DrawCard();
+            HandZone.AddCard(c);
+        }
+        public static void PrepareGame()
+        {
+            DeckZone = new DeckZone(Deck);
+            LifePoints = 8000;
+            
+        }
+
+    }
+}
---
+++ YGOCardSearch/Pages/Game.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YGOCardSearch.Data.Models;
+
+namespace YGOCardSearch.Pages
+{
+    public class GameModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+        }
+        public static void PrepareGame()
+        {
+
+        }
+    }
+}
