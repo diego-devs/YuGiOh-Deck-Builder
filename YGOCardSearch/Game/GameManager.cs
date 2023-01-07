1674688092863107c45bc4835bed3a3bc3b05755
Diego Díaz Mendoza <d_diazmendoza@hotmail.com>
2023-01-07 02:45:02 -0600

    added IDuelist and models from java proj

--- YGOCardSearch/Game/GameManager.cs
+++ YGOCardSearch/Game/GameManager.cs
@@ -2,5 +2,14 @@
 {
     public class GameManager
     {
+        public int turnNumber { get; set; }
+        Player ActivePlayer { get; set; }
+
+
+        public static void PrepareGame()
+        {
+
+        }
+        
     }
 }
