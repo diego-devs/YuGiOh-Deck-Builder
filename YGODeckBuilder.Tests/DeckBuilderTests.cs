beeb36e6ca5eb456fd6c1089680644d17646c8d6
Diego Díaz Mendoza <d_diazmendoza@hotmail.com>
2024-05-20 20:32:26 -0600

    First Unit tests for DeckBuilder app

---
+++ YGODeckBuilder.Tests/DeckBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YGODeckBuilder.Data.Models;
+using YGODeckBuilder.Data;
+using YGODeckBuilder.Pages;
+using Microsoft.Extensions.Configuration;
+
+namespace YGODeckBuilder.Tests
+{
+    public class DeckBuilderTests
+    {
+        [Fact]
+        public async Task OnGetAsync_DeckFileNameNotEmpty_DeckLoaded()
+        {
+            // Arrange
+            var mockContext = new Mock<YgoContext>();
+            var mockConfig = new Mock<IConfiguration>();
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var deck = new Deck { DeckName = "test" };
+            var deckUtility = new Mock<DeckUtility>(mockContext.Object, mockConfig.Object);
+            deckUtility.Setup(d => d.LoadDeckAsync(It.IsAny<string>())).ReturnsAsync(deck);
+
+            var deckBuilder = new DeckBuilder(mockContext.Object, mockConfig.Object, mockHttpContextAccessor.Object)
+            {
+                DeckFileName = "test.ydk"
+            };
+
+            // Act
+            var result = await deckBuilder.OnGetAsync();
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+            Assert.NotNull(deckBuilder.Deck);
+        }
+    }
+}
---
+++ YGODeckBuilder.Tests/DecksManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YGODeckBuilder.Data;
+using YGODeckBuilder.Data.Models;
+using YGODeckBuilder.Pages;
+using Xunit;
+using Microsoft.Extensions.Configuration;
+
+namespace YGODeckBuilder.Tests
+{
+    public class DecksManagerTests
+    {
+        [Fact]
+        public void OnGet_LoadsDecksPreview()
+        {
+            // Arrange
+            var mockContext = new Mock<YgoContext>();
+            var mockConfig = new Mock<IConfiguration>();
+            var mockDeckUtility = new Mock<DeckUtility>(mockContext.Object, mockConfig.Object);
+            mockDeckUtility.Setup(d => d.LoadDecksPreview()).Returns(new List<DeckPreview>());
+
+            var decksManager = new DecksManager(mockContext.Object, mockConfig.Object);
+            decksManager.Decks = new List<DeckPreview>();
+
+            // Act
+            decksManager.OnGet();
+
+            // Assert
+            Assert.NotNull(decksManager.Decks);
+        }
+    }
+}
