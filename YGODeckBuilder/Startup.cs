9dd7061cee4b1559bb0d4337eedd1a78a4a1305b
Diego Díaz Mendoza <diego.devs@outlook.com>
2025-01-13 12:56:31 -0600

    last build

--- YGODeckBuilder/Startup.cs
+++ YGODeckBuilder/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using YGODeckBuilder.Data;
-using YGODeckBuilder.Data.SharedData;
 using YGODeckBuilder.DataProviders;
 using YGODeckBuilder.Interfaces;
 
